7904a4fedfbced3b3c9fcdf523ba3c0cdf6a2abb
vuongvantu <vuongvantu2001x@gmail.com>
2021-08-27 10:08:09 +0700

    create

--- src/app/conponent/player-create/player-create.component.ts
+++ src/app/conponent/player-create/player-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {PlayerService} from '../../service/player.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-player-create',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-create.component.scss']
 })
 export class PlayerCreateComponent implements OnInit {
-
-  constructor() { }
+// @ts-ignore
+  playerForm: FormGroup = new FormGroup({
+    name: new FormControl(),
+    champ: new FormControl(),
+    kda: new FormControl(),
+    des: new FormControl(),
+  });
+  constructor(private playerService: PlayerService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  // tslint:disable-next-line:typedef
+  submit() {
+    const player = this.playerForm.value;
+    this.playerService.savePlayer(player).subscribe(() => {
+      this.playerForm.reset();
+      alert('Tạo thành công');
+      this.router.navigate(['/players']);
+    }, e => {
+      console.log(e);
+    });
+  }
 }
